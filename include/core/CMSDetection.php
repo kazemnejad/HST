116ec61ab08ba76bc3b16392485bf8b84004a3e6
MaKa <amirhosein@maka-lap.(none)>
2013-02-21 09:39:20 +0330

    CMSDetection module started

--- include/core/CMSDetection.php
+++ include/core/CMSDetection.php
@@ -1,9 +1,34 @@
 <?php
-class CMSClass{
+require_once 'requestManager.php';
+abstract class CMSDetector{
 	private $cachedArray;
 	
 	public function __construct(){
+		$this->cachedArray = array();
+	}
+	
+	protected abstract function detect($url);
+	
+	private function getPageContent($url){
+		if (isset($this->cachedArray[$url]))
+			return $this->cachedArray[$url];
+		$req = new RequestManager();
+		
+		$response = $req->sendRequest(array(), 'get', $url);
+		$this->cachedArray[$url] = $response;
+		return $response;
+	}
+};
+
+class JoomlaDetector extends CMSDetector{
+	
+	protected function detect($url){
 		
 	}
 	
+	private function checkMeta(){
+		
+	}
+	
+	private 
 };
